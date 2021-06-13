616cb94daba1d01500111aa599e591203f14991c
Carlos Eduardo Cavalcanti <69211390+carloseduardocsf@users.noreply.github.com>
2021-06-12 22:08:34 -0300

    automatic_email

---
+++ send_email.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+import smtplib
+import email.message
+
+def enviar_email():  
+    corpo_email = """
+    <p>Olá</p>
+    <p>Segue meu primeiro email automático.</p>
+    <p>Abs</p>
+    <p>Cadu</p>
+    """
+
+    msg = email.message.Message()
+    msg['Subject'] = "Email Automático"
+    msg['From'] = 'caducavalcanti01@gmail.com'
+    msg['To'] = 'caducavalcanti01@gmail.com'
+    password = 'minha_senha' 
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(corpo_email )
+
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+    # Login Credentials for sending the mail
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+    print('Email enviado')
+
+
+# In[18]:
+
+
+enviar_email()
+
+
+# In[ ]:
+
+
+
+
